420d9e06d07ab29de6dafe90017b6600b36b16db
ryanespin <ryanespin@gmail.com>
2017-03-20 19:47:08 -0400

    Got login page up.

--- frontend/src/components/App/index.js
+++ frontend/src/components/App/index.js
@@ -4,6 +4,9 @@ import { amber500, amber700, amber400 } from 'material-ui/styles/colors';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import AppBar from 'material-ui/AppBar';
+import Divider from 'material-ui/Divider';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import Home from '../Home';
 import Login from '../Login';
@@ -22,6 +25,19 @@ class App extends Component {
         injectTapEventPlugin();
     }
 
+    constructor(props) {
+        super(props);
+        this.state = {open: false};
+    }
+
+    handleToggle() {
+        this.setState({open: !this.state.open});
+    }
+
+    handleClose() {
+        this.setState({open: false});
+    }
+
     render() {
 
         return (
@@ -32,7 +48,18 @@ class App extends Component {
                             <AppBar
                                 title="Gloomhaven.Party"
                                 iconClassNameRight="muidocs-icon-navigation-expand-more"
+                                onLeftIconButtonTouchTap={this.handleToggle}
                             />
+                            <Drawer
+                                docked={false}
+                                open={this.state.open}
+                                onRequestChange={(open) => this.setState({open})}
+                            >
+                                <MenuItem onTouchTap={this.handleClose}>Characters</MenuItem>
+                                <MenuItem onTouchTap={this.handleClose}>Parties</MenuItem>
+                                <Divider />
+                                <MenuItem onTouchTap={this.handleClose}>Settings</MenuItem>
+                            </Drawer>
                         </div>
                         <Route exact path="/" component={ Home } />
                         <Route exact path="/Login" component={ Login } />
