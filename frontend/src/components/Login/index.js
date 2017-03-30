412ce95971a5affb6b3ec0477d242440d4dd9d8c
Devin Marsh <devin.m65@gmail.com>
2017-03-30 17:48:52 -0400

    Correct user email comparison in login screen

--- frontend/src/components/Login/index.js
+++ frontend/src/components/Login/index.js
@@ -53,7 +53,7 @@ class Login extends Component {
             <div className="login splash-screen">
                 <h1 className="logo">Gloomhaven.Party</h1>
                 { 
-                    user.email === 'undefined' ?
+                    typeof user.email === 'undefined' ?
                     <GoogleLogin { ...this.googleLoginConfig } style={ {} } className="login__google-signin-container">
                         <div className="login__google-signin"></div>
                     </GoogleLogin> : null
