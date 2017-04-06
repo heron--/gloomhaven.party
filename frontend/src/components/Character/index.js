00a9b5e1d0f6f6f14cf0b293af0ff7db6ed53673
ryanespin <ryanespin@gmail.com>
2017-04-05 20:28:02 -0400

    Added the select control in there as well.

--- frontend/src/components/Character/index.js
+++ frontend/src/components/Character/index.js
@@ -1,40 +1,88 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Card, CardHeader, CardText } from 'material-ui/Card';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
 import TextField from 'material-ui/TextField';
 import '../FormControl/form-control.scss';
 
 var styles = {
-    floatingLabelStyle:{
-        marginTop: '-12px',
-        paddingLeft: '20px',
+    input:{
+        floatingLabelStyle:{
+            marginTop: '-12px',
+            paddingLeft: '20px',
+        },
+        floatingLabelShrinkStyle:{
+            transform: 'scale(0.6) translate(13px, -30px)'
+        },
+        inputStyle:{
+            backgroundColor: '#fff',
+            fontFamily: '"Fjord One", serif',
+            fontSize: '20px',
+            marginTop: '0',
+            paddingLeft: '20px',
+            paddingRight: '20px',
+            paddingTop: '12px',
+        },
+        style:{
+            width: '100%'
+        },
+        underlineStyle:{
+            bottom: '-2px',
+            borderColor: 'transparent'
+        },
+        underlineFocusStyle:{
+            right: '0',
+            width: 'calc(100% - 12px)'
+        }
     },
-    floatingLabelShrinkStyle:{
-        transform: 'scale(0.6) translate(13px, -30px)'
-    },
-    inputStyle:{
-        backgroundColor: '#fff',
-        fontFamily: '"Fjord One", serif',
-        fontSize: '20px',
-        marginTop: '0',
-        paddingLeft: '20px',
-        paddingRight: '20px',
-        paddingTop: '12px',
-    },
-    textFieldStyle:{
-        width: '100%'
-    },
-    underlineStyle:{
-        bottom: '-2px',
-        borderColor: 'transparent'
-    },
-    underlineFocusStyle:{
-        right: '0',
-        width: 'calc(100% - 12px)'
+    classSelect:{
+        hintStyle:{
+            bottom: 'calc(50% - 12px)',
+            fontFamily: '"Medula One", cursive',
+            fontSize: '30px',
+            paddingLeft: '20px',
+            zIndex: '10'
+        },
+        iconStyle:{
+            right: '-20px',
+            top: '12px'
+        },
+        labelStyle:{
+            fontFamily: '"Medula One", cursive',
+            fontSize: '30px',
+            top: '7px'
+        },
+        menuStyle:{
+            backgroundColor: '#fff',
+            boxSizing: 'border-box',
+            fontFamily: '"Fjord One", serif',
+            fontSize: '20px',
+            marginTop: '0',
+            paddingLeft: '20px',
+            paddingRight: '20px',
+        },
+        style:{
+            height: '72px',
+            width: '100%'
+        }
     }
 };
 
 class Character extends Component {
+
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.state = {
+            value: null,
+        };
+    }
+
+    handleChange(event, index, value) {
+        this.setState({value});
+    }
+
     render() {
         const {
             characterClasses
@@ -42,29 +90,40 @@ class Character extends Component {
 
         return (
             <Card>
-            <CardHeader
-                title="My Character Name"
-                subtitle="My Character Class"
-                avatar="images/jsa-128.jpg"
-            />
-            <CardText>
-                <TextField
-                    className="form-control"
-                    hintText="Hint Text"
-                    floatingLabelText="Character Name"
-                    floatingLabelStyle={styles.floatingLabelStyle}
-                    floatingLabelShrinkStyle={styles.floatingLabelShrinkStyle}
-                    style={styles.textFieldStyle}
-                    inputStyle={styles.inputStyle}
-                    underlineStyle={styles.underlineStyle}
-                    underlineFocusStyle={styles.underlineFocusStyle}
+                <CardHeader
+                    title="My Character Name"
+                    subtitle="My Character Class"
+                    avatar="images/jsa-128.jpg"
                 />
-            </CardText>
-                <select>
-                    {
-                        characterClasses.map(c => <option key={ c.id } value={ c.id }>{ c.name }</option>)
-                    } 
-                </select>
+                <CardText>
+                    <SelectField
+                        className="form-control"
+                        hintStyle={styles.classSelect.hintStyle}
+                        hintText="Class"
+                        iconStyle={styles.classSelect.iconStyle}
+                        labelStyle={styles.classSelect.labelStyle}
+                        menuStyle={styles.classSelect.menuStyle}
+                        onChange={this.handleChange}
+                        style={styles.classSelect.style}
+                        underlineStyle={styles.input.underlineStyle}
+                        value={this.state.value}
+                    >
+                        {
+                            characterClasses.map(c => <MenuItem key={ c.id } value={ c.id } primaryText={ c.name } />)
+                        } 
+                    </SelectField>
+                    <TextField
+                        className="form-control"
+                        hintText="Hint Text"
+                        floatingLabelText="Name"
+                        floatingLabelStyle={styles.input.floatingLabelStyle}
+                        floatingLabelShrinkStyle={styles.input.floatingLabelShrinkStyle}
+                        style={styles.input.style}
+                        inputStyle={styles.input.inputStyle}
+                        underlineStyle={styles.input.underlineStyle}
+                        underlineFocusStyle={styles.input.underlineFocusStyle}
+                    />
+                </CardText>
             </Card>
         );
     } 
