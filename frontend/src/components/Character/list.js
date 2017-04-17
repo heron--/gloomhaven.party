217837b4438f467aa3a6cddc4f4f4770a43bc1f2
ryanespin <ryanespin@gmail.com>
2017-04-16 21:40:47 -0400

    Got the browse page in.

--- frontend/src/components/Character/list.js
+++ frontend/src/components/Character/list.js
@@ -2,22 +2,50 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { Card } from 'material-ui/Card';
+import { Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn } from 'material-ui/Table';
+import FontIcon from 'material-ui/FontIcon';
 
 const CharacterList = ({
     characters
 }) => {
     return (
-        <Card style={{ maxWidth: '600px', margin: '10px auto' }}>
-            {
-                characters.map(c => <Link to={ `/character/${ c.id }/edit` } key={ c.id } style={{ padding: '20px 12px', display: 'block' }}>{c.name}</Link>)
-            }
+        <Card style={{ minWidth: '300px', maxWidth: '80%', margin: '10px auto' }}>
+            <Table>
+                <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
+                    <TableRow>
+                        <TableHeaderColumn style={{ width: '30px' }}>Class</TableHeaderColumn>
+                        <TableHeaderColumn>Name</TableHeaderColumn>
+                        <TableHeaderColumn>Level</TableHeaderColumn>
+                        <TableHeaderColumn>XP</TableHeaderColumn>
+                        <TableHeaderColumn>Gold</TableHeaderColumn>
+                        <TableHeaderColumn>Owner</TableHeaderColumn>
+                    </TableRow>
+                </TableHeader>
+                <TableBody displayRowCheckbox={false}>
+                    {
+                        characters.map(c =>
+                            <TableRow>
+                                <TableRowColumn style={{ width: '30px', textAlign: 'center' }}><FontIcon className="gloomhaven-icon-class-01" /></TableRowColumn>
+                                <TableRowColumn>
+                                    <Link to={ `/character/${ c.id }/edit` } key={ c.id }>{c.name}</Link>
+                                </TableRowColumn>
+                                <TableRowColumn>{c.level}</TableRowColumn>
+                                <TableRowColumn>{c.experienceNotes}</TableRowColumn>
+                                <TableRowColumn>{c.goldNotes}</TableRowColumn>
+                                <TableRowColumn>{c.owner}</TableRowColumn>
+                            </TableRow>
+                        )
+                    }
+                </TableBody>
+            </Table>
         </Card>
     );
 }
 
 function mapStateToProps(state) {
     return {
-        characters: state.character.userCharacters
+        characters: state.character.userCharacters,
+        characterClasses: state.character.classes
     }
 }
 
