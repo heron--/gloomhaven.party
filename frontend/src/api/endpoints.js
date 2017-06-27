acc9a2d6815b703916e0bb815c149b0fd81a8001
Devin Marsh <devin.m65@gmail.com>
2017-06-26 23:18:09 -0400

    Change all endpoints to https

--- frontend/src/api/endpoints.js
+++ frontend/src/api/endpoints.js
@@ -8,21 +8,21 @@ export const devEndpoints = {
 };
 
 const stageEndpoints = {
-    login: 'http://stage.gloomhaven.party/api/auth/login',
-    logout: 'http://stage.gloomhaven.party/api/auth/logout',
-    getClasses: 'http://stage.gloomhaven.party/api/character/classes',
-    checkSession: 'http://stage.gloomhaven.party/api/user/check-session',
-    updateCharacter: 'http://stage.gloomhaven.party/api/character/',
-    createCharacter: 'http://stage.gloomhaven.party/api/character/' 
+    login: 'https://stage.gloomhaven.party/api/auth/login',
+    logout: 'https://stage.gloomhaven.party/api/auth/logout',
+    getClasses: 'https://stage.gloomhaven.party/api/character/classes',
+    checkSession: 'https://stage.gloomhaven.party/api/user/check-session',
+    updateCharacter: 'https://stage.gloomhaven.party/api/character/',
+    createCharacter: 'https://stage.gloomhaven.party/api/character/' 
 };
 
 const prodEndpoints = {
-    login: 'http://gloomhaven.party/api/auth/login',
-    logout: 'http://gloomhaven.party/api/auth/logout',
-    getClasses: 'http://gloomhaven.party/api/character/classes',
-    checkSession: 'http://gloomhaven.party/api/user/check-session',
-    updateCharacter: 'http://gloomhaven.party/api/character/',
-    createCharacter: 'http://gloomhaven.party/api/character/' 
+    login: 'https://gloomhaven.party/api/auth/login',
+    logout: 'https://gloomhaven.party/api/auth/logout',
+    getClasses: 'https://gloomhaven.party/api/character/classes',
+    checkSession: 'https://gloomhaven.party/api/user/check-session',
+    updateCharacter: 'https://gloomhaven.party/api/character/',
+    createCharacter: 'https://gloomhaven.party/api/character/' 
 };
 
 const exportEndpoints = window.location.hostname.indexOf('stage.gloomhaven') === -1 ? prodEndpoints : stageEndpoints;
